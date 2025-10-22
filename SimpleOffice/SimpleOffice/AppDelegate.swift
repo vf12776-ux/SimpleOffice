280ebc7f0ceb99b50ecb8bf60de4eff9fff21fda
vf12776-ux <vf12776@gmail.com>
2025-10-22 23:04:02 +0300

    Update AppDelegate.swift

--- SimpleOffice/SimpleOffice/AppDelegate.swift
+++ SimpleOffice/SimpleOffice/AppDelegate.swift
@@ -1,28 +1,25 @@
-//
-//  AppDelegate.swift
-//  SimpleOffice
-//
-//  Created by User on 19.10.2025.
-//
-
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Устанавливаем иконку приложения
-        if let imagePath = Bundle.main.path(forResource: "icon5", ofType: "png"),
-           let iconImage = NSImage(contentsOfFile: imagePath) {
-            NSApplication.shared.applicationIconImage = iconImage
-        }
+        // Настройка приложения
+        setupAppearance()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        // Сохранение состояния при выходе
     }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
+    }
+    
+    private func setupAppearance() {
+        // Настройка темной темы
+        if #available(macOS 10.14, *) {
+            NSApp.appearance = NSAppearance(named: .darkAqua)
+        }
     }
 }
